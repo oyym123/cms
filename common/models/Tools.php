f5f290781f46de863b9842acd97b7b2353ec02fc
oyym <1052016884>
2020-06-23 10:09:35 +0800

    修改

--- common/models/Tools.php
+++ common/models/Tools.php
@@ -20,4 +20,34 @@ class Tools extends \yii\db\ActiveRecord
         $path = PHP_OS == "Linux" ? '/www/wwwroot/logs/cms/' : 'E:/phpstudy_pro/WWW/';
         file_put_contents($path . $title, $res . PHP_EOL . PHP_EOL, FILE_APPEND);
     }
+
+    public static function curlPost($url, $data)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // post数据
+        curl_setopt($ch, CURLOPT_POST, 1);
+        // post的变量
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+
+    public static function curlGet($url)
+    {
+        $ch = curl_init();
+        //设置选项，包括URL
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        //执行并获取HTML文档内容
+        $output = curl_exec($ch);
+        //释放curl句柄
+        curl_close($ch);
+        //打印获得的数据
+        return $output;
+    }
 }
