639dcabf99a93bac90209122bf41af7cc6fd176d
oyym <1052016884>
2020-06-28 17:52:54 +0800

    修改移动端tags
    修改移动端tags

--- frontend/web/test.php
+++ frontend/web/test.php
@@ -1,57 +1,31 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>标签大全-何马外教网</title>
-    <meta name="description" content="[!--pagedes--]"/>
-    <meta name="keywords" content="[!--pagekey--]"/>
-    <link href="/static/css/home.css" rel="stylesheet" type="text/css">
-    <link href="/static/css/main.css" rel="stylesheet" type="text/css">
-    <script src="/static/js/checkmobile.js"></script>
+    <title>TAGS标签大全-何马英语培训网</title>
+    <meta name="Keywords" content="TAGS标签大全"/>
+    <meta name="Description" content=""/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="applicable-device" content="mobile"/>
+    <link rel="stylesheet" href="/style/css/aq.css"/>
+    <link type="image/x-icon" rel="shortcut icon" href="/favicon.ico">
+    <link rel="canonical" href="https://m.jk8818.com/tags.html">
 </head>
 <body>
-<div class="toplineBox">
-    <div class="topline">
-        <div class="ss5">怎么学英语？就找何马外教网！我们分享怎么学英语最快最有效的方法！</div>
-        <div class="toplinebox1">
-            <div class="flagbox"><a href="/sitemap.html" class="clearleft" class="wz">网站地图</a><a href="/tags.php"
-                                                                                                 class="clearleft"
-                                                                                                 class="wz">TAGS</a><a
-                        href="/sitemap.xml" class="clearleft" class="wz">XML</a><a href="/sitemap.txt" class="clearleft"
-                                                                                   class="wz">TXT</a></div>
-        </div>
-    </div>
+<div class="top"><a href="/">何马英语培训网</a></div>
+<ul class="nav">
+    <li><a href="/chuyiyingyu/">初一英语</a></li>
+    <li><a href="/chueryingyu/">初二英语</a></li>
+    <li><a href="/chusanyingyu/">初三英语</a></li>
+    <li><a href="/waijiao1dui1/">外教1对1</a></li>
+</ul>
+<div class="both"></div>
+<div class="map">
+    位置：<a href="/">何马英语培训网</a> > <a href="/tags.html">标签大全</a>
 </div>
-<div class="menulinebox">
-    <div class="menubox">
-        <div class="logobox">
-            <a href="/"><img src="/static/picture/logo.png" alt="何马外教网" height="60" style="margin-top:11px"></a>
-        </div>
-        <div class="rightmenubox">
-            <a href="/" class="">何马外教网</a>
-            |<a href="/xxsyy" class="hh1" title="小学生英语">
-                小学生英语 </a>
-            |<a href="/qsnyy" class="hh2" title="青少年英语">
-                青少年英语 </a>
-            |<a href="/yyky" class="hh3" title="英语口语">
-                英语口语 </a>
-            |<a href="/yyzsd" class="hh5" title="英语知识点">
-                英语知识点 </a>
-            |<a href="/zxyy" class="hh6" title="自学英语">
-                自学英语 </a>
-            |<a href="/zaixianyy" class="hh7" title="在线英语">
-                在线英语 </a>
-            |<a href="/wjyy" class="hh8" title="外教">
-                外教 </a>
-            |<a href="/xxzx" class="hh9" title="学习资讯">
-                学习资讯 </a>
-        </div>
-    </div>
-</div>
-<div class="map w120 m0a mt90">标签大全</div>
-
-<div class="w120 m0a tags">
-    <div id="content"><br/><div class="map w120 m0a mt90">初一英语</div><br/><a href="/e/tags/?tagid=1">初一英语</a><br/><a href="/e/tags/?tagid=7">初一英语知识点</a><br/><br/><div class="map w120 m0a mt90">初二英语</div><br/><a href="/e/tags/?tagid=2">初二英语</a><br/><a href="/e/tags/?tagid=2">初二英语差还能赶上吗</a><br/><a href="/e/tags/?tagid=3">初二英语成绩差怎么办</a><br/><a href="/e/tags/?tagid=4">初二英语重点知识归纳</a><br/><a href="/e/tags/?tagid=4">初二英语语法归纳</a><br/><a href="/e/tags/?tagid=5">初二英语作文</a><br/><a href="/e/tags/?tagid=5">初二英语阅读理解100篇</a><br/><a href="/e/tags/?tagid=6">初二英语知识点</a><br/><a href="/e/tags/?tagid=6">初二英语下册笔记整理</a><br/><br/><div class="map w120 m0a mt90">初三英语</div><br/><a href="/e/tags/?tagid=8">初三英语</a><br/><a href="/e/tags/?tagid=8">初三英语知识点大总结</a><br/><a href="/e/tags/?tagid=9">初三英语怎么快速提高</a><br/><a href="/e/tags/?tagid=10">英语简单对话两人十句</a><br/><a href="/e/tags/?tagid=11">初三中考英语高频考点总结</a><br/><a href="/e/tags/?tagid=12">初三英语差生逆袭计划</a><br/><a href="/e/tags/?tagid=12">111</a><br/><br/><div class="map w120 m0a mt90">外教1对1</div><br/></div>
+<div class="tags"><br/><div class="map w120 m0a mt90"><strong>初一英语</strong></div><br/><a href="/e/tags/?tagid=1">初一英语</a><br/><a href="/e/tags/?tagid=5">初一英语知识点</a><br/><br/><div class="map w120 m0a mt90"><strong>初二英语</strong></div><br/><a href="/e/tags/?tagid=30">初二英语</a><br/><a href="/e/tags/?tagid=60">初二英语差还能赶上吗</a><br/><a href="/e/tags/?tagid=38">初二英语成绩差怎么办</a><br/><a href="/e/tags/?tagid=45">初二英语重点知识归纳</a><br/><a href="/e/tags/?tagid=32">初二英语语法归纳</a><br/><a href="/e/tags/?tagid=35">初二英语作文</a><br/><a href="/e/tags/?tagid=47">初二英语阅读理解100篇</a><br/><a href="/e/tags/?tagid=31">初二英语知识点</a><br/><a href="/e/tags/?tagid=50">初二英语下册笔记整理</a><br/><br/><div class="map w120 m0a mt90"><strong>初三英语</strong></div><br/><a href="/e/tags/?tagid=90">初三英语</a><br/><a href="/e/tags/?tagid=61">初三英语知识点大总结</a><br/><a href="/e/tags/?tagid=69">初三英语怎么快速提高</a><br/><a href="/e/tags/?tagid=73">英语简单对话两人十句</a><br/><a href="/e/tags/?tagid=91">初三中考英语高频考点总结</a><br/><a href="/e/tags/?tagid=71">初三英语差生逆袭计划</a><br/><a href="/e/tags/?tagid=92">111</a><br/><br/><div class="map w120 m0a mt90"><strong>外教1对1</strong></div><br/><br/><div class="map w120 m0a mt90"><strong>其他</strong></div><br/><a href="/e/tags/?tagid=2">初一英语单词</a><br/><a href="/e/tags/?tagid=3">初一英语上册重点归纳</a><br/><a href="/e/tags/?tagid=4">初一英语听力在线听</a><br/><a href="/e/tags/?tagid=6">初一英语作文</a><br/><a href="/e/tags/?tagid=7">初一英语语法重点总结</a><br/><a href="/e/tags/?tagid=8">初一英语书</a><br/><a href="/e/tags/?tagid=9">初一英语书上册人教版</a><br/><a href="/e/tags/?tagid=10">初一英语试卷</a><br/><a href="/e/tags/?tagid=11">初一上册英语课本内容</a><br/><a href="/e/tags/?tagid=12">初一英语下册重点归纳</a><br/><a href="/e/tags/?tagid=13">英语演讲稿1分钟带翻译</a><br/><a href="/e/tags/?tagid=14">初一英语情景剧</a><br/><a href="/e/tags/?tagid=15">初一英语下册课本</a><br/><a href="/e/tags/?tagid=16">七上英语书电子课本人教版</a><br/><a href="/e/tags/?tagid=17">初一英语上册ppt课件</a><br/><a href="/e/tags/?tagid=18">初一下册英语试题及答案</a><br/><a href="/e/tags/?tagid=19">初一英语上册课本图片</a><br/><a href="/e/tags/?tagid=20">初一英语单词上册</a><br/><a href="/e/tags/?tagid=21">初中英语不好怎么补救</a><br/><a href="/e/tags/?tagid=22">初一上册英语书听力</a><br/><a href="/e/tags/?tagid=23">初中七年级下册英语知识点</a><br/><a href="/e/tags/?tagid=24">初一上册英语</a><br/><a href="/e/tags/?tagid=25">初一英语听力在线播放</a><br/><a href="/e/tags/?tagid=26">初一英语课本电子版</a><br/><a href="/e/tags/?tagid=27">初一英语知识点梳理</a><br/><a href="/e/tags/?tagid=28">初一英语重点知识归纳</a><br/><a href="/e/tags/?tagid=29">初一英语下册课文</a><br/><a href="/e/tags/?tagid=33">初二英语单词</a><br/><a href="/e/tags/?tagid=34">初二英语课文原文朗读</a><br/><a href="/e/tags/?tagid=36">初二英语课本</a><br/><a href="/e/tags/?tagid=37">初二英语听力</a><br/><a href="/e/tags/?tagid=39">初二英语上册</a><br/><a href="/e/tags/?tagid=40">初二英语网课视频</a><br/><a href="/e/tags/?tagid=41">八年级英语人教版课件</a><br/><a href="/e/tags/?tagid=42">初中英语美文100篇</a><br/><a href="/e/tags/?tagid=43">初二八下英语作文范文</a><br/><a href="/e/tags/?tagid=44">初二年级英语思维导图</a><br/><a href="/e/tags/?tagid=46">八年级英语课本电子版</a><br/><a href="/e/tags/?tagid=48">初二英语上册单词</a><br/><a href="/e/tags/?tagid=49">免费初二英语讲课视频</a><br/><a href="/e/tags/?tagid=51">初二英语课文原文</a><br/><a href="/e/tags/?tagid=52">初二英语下册电子课本</a><br/><a href="/e/tags/?tagid=53">初二英语下册课本单词</a><br/><a href="/e/tags/?tagid=54">初二下册英语重点知识</a><br/><a href="/e/tags/?tagid=55">英语初中三年知识归纳</a><br/><a href="/e/tags/?tagid=56">初二下英语知识点汇总</a><br/><a href="/e/tags/?tagid=57">初二英语下册课本人教版</a><br/><a href="/e/tags/?tagid=58">初二八下英语课文</a><br/><a href="/e/tags/?tagid=59">初中英语单词免费听读</a><br/><a href="/e/tags/?tagid=62">初三英语单词表全一册</a><br/><a href="/e/tags/?tagid=63">初三英语知识点</a><br/><a href="/e/tags/?tagid=64">初三英语书</a><br/><a href="/e/tags/?tagid=65">初三英语语法归纳总结</a><br/><a href="/e/tags/?tagid=66">初三英语怎么说</a><br/><a href="/e/tags/?tagid=67">初三英语作文</a><br/><a href="/e/tags/?tagid=68">初三英语课本人教版</a><br/><a href="/e/tags/?tagid=70">初三英语单词</a><br/><a href="/e/tags/?tagid=72">初三英语语法ppt</a><br/><a href="/e/tags/?tagid=74">初三英语语法知识</a><br/><a href="/e/tags/?tagid=75">中考英语最难城市排名</a><br/><a href="/e/tags/?tagid=76">单招英语必背知识点</a><br/><a href="/e/tags/?tagid=77">初三英语听力试题</a><br/><a href="/e/tags/?tagid=78">英语初三模拟试题</a><br/><a href="/e/tags/?tagid=79">初三英语试题及答案解析</a><br/><a href="/e/tags/?tagid=80">初中英语必考知识点</a><br/><a href="/e/tags/?tagid=81">中考英语知识点归纳</a><br/><a href="/e/tags/?tagid=82">初中英语中考考点梳理</a><br/><a href="/e/tags/?tagid=83">中考英语高频考点总结</a><br/><a href="/e/tags/?tagid=84">初中英语主要学哪些内容</a><br/><a href="/e/tags/?tagid=85">中考英语语法考点总结</a><br/><a href="/e/tags/?tagid=86">初三英语中考模拟试卷</a><br/><a href="/e/tags/?tagid=87">初三英语重点语法</a><br/><a href="/e/tags/?tagid=88">初三英语真题卷子</a><br/><a href="/e/tags/?tagid=89">初三英语模拟试题及答案</a><br/><a href="/e/tags/?tagid=93">555</a><br/></div>
 <script>
     (function () {
         var bp = document.createElement('script');
@@ -63,6 +37,15 @@
         }
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(bp, s);
+    })();
+</script>
+<script>
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?24d2aa604f61fcc7afbd78e73aecdf83";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
     })();
 </script>
 </body>
