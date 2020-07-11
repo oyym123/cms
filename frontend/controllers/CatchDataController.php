54e5fc733d1acd32d3f6316d605d8dd9e6e8d96e
oyym <1052016884>
2020-07-11 17:18:01 +0800

    抓取词
    抓取词

--- frontend/controllers/CatchDataController.php
+++ frontend/controllers/CatchDataController.php
@@ -4,6 +4,7 @@
 namespace frontend\controllers;
 
 
+use common\models\Tools;
 use yii\web\Controller;
 
 class CatchDataController extends Controller
@@ -14,4 +15,15 @@ class CatchDataController extends Controller
     }
 
 
+    /** 抓取搜狗微信数据 */
+    public function actionSgwx()
+    {
+        $keywords = ['英语'];
+        foreach ($keywords as $keyword) {
+            $url = 'https://weixin.sogou.com/weixin?type=2&s_from=input&query=英语&ie=utf8&_sug_=y&_sug_type_=&w=01019900&sut=2456&sst0=1594457792745&lkt=1,1594457792644,1594457792644&page=' . '2';
+
+            $res = Tools::curlGet($url);
+            print_r($res);
+        }
+    }
 }
