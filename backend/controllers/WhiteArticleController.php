0d1cfa8a2bc40f5af9f19a56a3c7404a7be80f2c
oyym <1052016884>
2020-07-16 13:07:14 +0800

    修改
    修改

--- backend/controllers/WhiteArticleController.php
+++ backend/controllers/WhiteArticleController.php
@@ -105,7 +105,7 @@ class WhiteArticleController extends Controller
                     $model->title_img = $imgInfo['url'] . $imgView;
                     $data['title_img'] = $imgInfo['url'] . $imgView;
                 } else {
-                    if (strpos($model->title_img, Yii::$app->params['img.thszxxdyw.org.cn']) !== false) {
+                    if (strpos($model->title_img, 'img.thszxxdyw.org.cn') !== false) {
                         $data['title_img'] = $model->title_img ?: '';
                     } else {
                         //当内容中有图片时，那么选择内容中的第一张图 并且将标题加上去alt     大小：240 * 180
